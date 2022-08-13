7cf4e815fcc566914301271066ea569c1afe2555
Wen Gong <gongwen1201@gmail.com>
2022-08-13 15:53:28 +1000

    Tic-Tac-Toe

--- js/refactor.js
+++ js/refactor.js
@@ -9,7 +9,7 @@ for (let i = 0; i < avenger.length; i++) {
     if (count < 9) {
         count++;
     } else if(count === 9){
-        if (allboxes[0] == allboxes[1] && allboxes[0]== allboxes[2]  || allboxes[0] == allboxes[3] && allboxes[0]== allboxes[6] || allboxes[0] == allboxes[4] && allboxes[0]== allboxes[8] || allboxes[1] == allboxes[4] && allboxes[1]== allboxes[7]|| allboxes[2] == allboxes[5] && allboxes[2] == allboxes[8] || allboxes[2] == allboxes[4] &&allboxes[2] == allboxes[6]|| allboxes[3] == allboxes[4] && allboxes[3] == allboxes[5] || allboxes[6] == allboxes[7] && allboxes[6] == allboxes[8]) {
+        if ( win()) {
         console.log('You WIn')  
         } else {
             $('h2').text("Draw");
@@ -19,12 +19,17 @@ for (let i = 0; i < avenger.length; i++) {
     });
     var allboxes = ['box1', 'box2', 'box3', 'box4', 'box5', 'box6', 'box7', 'box8', 'box9'];
     function printMousePos(){
+        function win() {
+            if(allboxes[0] == allboxes[1] && allboxes[0]== allboxes[2]  || allboxes[0] == allboxes[3] && allboxes[0]== allboxes[6] || allboxes[0] == allboxes[4] && allboxes[0]== allboxes[8] || allboxes[1] == allboxes[4] && allboxes[1]== allboxes[7]|| allboxes[2] == allboxes[5] && allboxes[2] == allboxes[8] || allboxes[2] == allboxes[4] &&allboxes[2] == allboxes[6]|| allboxes[3] == allboxes[4] && allboxes[3] == allboxes[5] || allboxes[6] == allboxes[7] && allboxes[6] == allboxes[8]){
+                return true;
+            }
+        }
         const position = $(this).attr('id');
         if (count % 2 != 0){
             $(`#img${position}`).attr('src', `images/${avenger[i]}.png`);
             $('h2').text("Thanos's Turn");
             allboxes[position - 1] = $(`#img${position}`).attr('src');    
-            if (allboxes[0] == allboxes[1] && allboxes[0]== allboxes[2]  || allboxes[0] == allboxes[3] && allboxes[0]== allboxes[6] || allboxes[0] == allboxes[4] && allboxes[0]== allboxes[8] || allboxes[1] == allboxes[4] && allboxes[1]== allboxes[7]|| allboxes[2] == allboxes[5] && allboxes[2] == allboxes[8] || allboxes[2] == allboxes[4] &&allboxes[2] == allboxes[6]|| allboxes[3] == allboxes[4] && allboxes[3] == allboxes[5] || allboxes[6] == allboxes[7] && allboxes[6] == allboxes[8] ) {
+            if (win()) {
                 $(`#${avenger[i]}-hidding`).css('visibility', 'visible');
                 $('h2').text(`${avenger[i]} Wins`);
                 console.log('You WIn')  
@@ -33,7 +38,7 @@ for (let i = 0; i < avenger.length; i++) {
             $(`#img${position}`).attr('src', 'images/thanos.jpeg'); 
             $('h2').text(`${avenger[i]}'s Turn`);
             allboxes[position - 1] = $(`#img${position}`).attr('src');    
-            if (allboxes[0] == allboxes[1] && allboxes[0]== allboxes[2]  || allboxes[0] == allboxes[3] && allboxes[0]== allboxes[6] || allboxes[0] == allboxes[4] && allboxes[0]== allboxes[8] || allboxes[1] == allboxes[4] && allboxes[1]== allboxes[7]|| allboxes[2] == allboxes[5] && allboxes[2] == allboxes[8] || allboxes[2] == allboxes[4] &&allboxes[2] == allboxes[6]|| allboxes[3] == allboxes[4] && allboxes[3] == allboxes[5] || allboxes[6] == allboxes[7] && allboxes[6] == allboxes[8] ) {
+            if (win()) {
                 $(`#T${avenger[i]}-hidding`).css('visibility', 'visible');
                 $('h2').text("Thanos Wins");
                 console.log('You WIn')
