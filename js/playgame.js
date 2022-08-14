1a39d9976e6058175c932f45912c84bce0e34064
Wen Gong <gongwen1201@gmail.com>
2022-08-14 21:21:37 +1000

    Tic-Tac-Toe

--- js/playgame.js
+++ js/playgame.js
@@ -9,26 +9,23 @@ for (let i = 0; i < avenger.length; i++) {
     function playgame(){
         $('h2').text(`${avenger[i]}'s Turn`);
         $(".box").on("click", printMousePos);
-    var count = 1;
-    $('body').on('click', '.box', function () {
-    if (count < 9) {
-        count++;
-    } else if(count === 9){
-        if ( win()) {
-        console.log('You WIn')  
-        } else {
-            $('h2').text("Draw");
-        console.log('Again')
-        } 
-    };
-    });
-    var allboxes = ['box1', 'box2', 'box3', 'box4', 'box5', 'box6', 'box7', 'box8', 'box9'];
-    function printMousePos(){
         function win() {
             if(allboxes[0] == allboxes[1] && allboxes[0]== allboxes[2]  || allboxes[0] == allboxes[3] && allboxes[0]== allboxes[6] || allboxes[0] == allboxes[4] && allboxes[0]== allboxes[8] || allboxes[1] == allboxes[4] && allboxes[1]== allboxes[7]|| allboxes[2] == allboxes[5] && allboxes[2] == allboxes[8] || allboxes[2] == allboxes[4] &&allboxes[2] == allboxes[6]|| allboxes[3] == allboxes[4] && allboxes[3] == allboxes[5] || allboxes[6] == allboxes[7] && allboxes[6] == allboxes[8]){
                 return true;
             }
         }
+    var count = 1;
+    $('body').on('click', '.box', function () {
+    if (count < 9) {
+        count++;
+    } else if(count === 9){
+        if ( win() !== true) {  
+            $('h2').text("Draw");
+        } 
+    };
+    });
+    var allboxes = ['box1', 'box2', 'box3', 'box4', 'box5', 'box6', 'box7', 'box8', 'box9'];
+    function printMousePos(){        
         const position = $(this).attr('id');
         if (count % 2 != 0){
             $(`#img${position}`).attr('src', `images/${avenger[i]}.png`);
@@ -37,7 +34,6 @@ for (let i = 0; i < avenger.length; i++) {
             if (win()) {
                 $(`#${avenger[i]}-hidding`).css('visibility', 'visible');
                 $('h2').text(`${avenger[i]} Wins`);
-                console.log('You WIn')  
             } 
         }else{
             $(`#img${position}`).attr('src', 'images/thanos.jpeg'); 
@@ -46,7 +42,6 @@ for (let i = 0; i < avenger.length; i++) {
             if (win()) {
                 $(`#T${avenger[i]}-hidding`).css('visibility', 'visible');
                 $('h2').text("Thanos Wins");
-                console.log('You WIn')
             } 
         }         
     }
